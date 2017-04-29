82361b58b32c2095c3300f0c904f4016ea5d7628
Anders Schuller <andersschuller@gmail.com>
2017-04-29 16:43:42 +0100

    Add common collection methods as Observable extension methods

--- src/main/scala/BaconExtensions.scala
+++ src/main/scala/BaconExtensions.scala
@@ -1,9 +1,46 @@
 import org.scalajs.dom
 
+import scala.language.higherKinds
+
 object BaconExtensions {
   implicit class EnrichedEventTarget(target: dom.EventTarget) {
     def asEventStream[T <: dom.Event](eventName: String): Bacon.EventStream[T] = {
       Bacon.fromEvent(target, eventName)
+    }
+  }
+
+  implicit class EnrichedObservable[T, O[U] <: Bacon.Observable[U]](observable: O[T]) {
+    def foreach[U](f: T => U): Unit = {
+      observable.onValue(v => f(v))
+    }
+
+    def flatten[A](implicit evidence: T <:< Bacon.Observable[A]): Bacon.EventStream[A] = {
+      observable.flatMap(t => t)
+    }
+
+    def withFilter(p: T => Boolean): O[T]#Self[T] = {
+      observable.filter(p)
+    }
+
+    def filterNot(p: T => Boolean): O[T]#Self[T] = {
+      observable.filter(v => !p(v))
+    }
+
+    def collect[A](pf: PartialFunction[T, A]): O[T]#Self[A] = {
+      observable.withHandler[A] { (dispatcher, event) =>
+        event match {
+          case n: Bacon.Next[T] if pf.isDefinedAt(n.value()) =>
+            dispatcher.push(new Bacon.Next(pf(n.value())))
+          case i: Bacon.Initial[T] if pf.isDefinedAt(i.value()) =>
+            dispatcher.push(new Bacon.Initial[A](pf(i.value())))
+          case e: Bacon.End =>
+            dispatcher.push(e)
+          case e: Bacon.Error =>
+            dispatcher.push(e)
+          case _ =>
+            ()
+        }
+      }
     }
   }
 }
--- src/test/scala/BaconExtensionsSuite.scala
+++ src/test/scala/BaconExtensionsSuite.scala
@@ -1,6 +1,9 @@
 import BaconExtensions._
 import org.scalajs.dom
 
+import scala.concurrent.Promise
+import scala.scalajs.js
+
 class BaconExtensionsSuite extends BaseSuite {
   override def newInstance = new BaconExtensionsSuite
 
@@ -15,5 +18,66 @@ class BaconExtensionsSuite extends BaseSuite {
     element.dispatchEvent(event)
 
     eventualValues.map(_ shouldEqual List(event))
+  }
+
+  test("Iterate over an Observable using foreach") {
+    val property = Bacon.constant(14)
+    val promise = Promise[Int]
+    property.foreach(promise.success)
+    withTimeout(promise.future).map(_ shouldEqual 14)
+  }
+
+  test("Flatten nested Observables") {
+    val property = Bacon.constant("foo")
+    val stream = Bacon.once(property)
+    val flattenedStream = stream.flatten
+    assertContainsValues(flattenedStream, List("foo"))
+  }
+
+  test("Filter values of an Observable using withFilter") {
+    val stream = Bacon.fromArray[String](js.Array("", "a", "b", "", "c"))
+    val filteredStream = stream.withFilter(_.nonEmpty)
+    assertContainsValues(filteredStream, List("a", "b", "c"))
+  }
+
+  test("Filter values of an Observable using filterNot") {
+    val stream = Bacon.fromArray[String](js.Array("", "a", "b", "", "c"))
+    val filteredStream = stream.filterNot(_.isEmpty)
+    assertContainsValues(filteredStream, List("a", "b", "c"))
+  }
+
+  test("Use Observables in a for comprehension") {
+    val stream = Bacon.fromArray[Int](js.Array(4, 2, 3, 5, 1))
+    def squaredStream(i: Int) = Bacon.once(i * i)
+    val promise = Promise[(Int, Int)]
+
+    for {
+      a <- stream
+      b <- squaredStream(a)
+      if a + b == 30
+    } promise.trySuccess((a, b))
+
+    withTimeout(promise.future) map (_ shouldEqual (5, 25))
+  }
+
+  test("Use Observables in a for comprehension with yield") {
+    val stream = Bacon.fromArray[Int](js.Array(9, 6, 1, 12))
+    def squaredProperty(i: Int) = Bacon.constant(i * i)
+
+    val combinedStream = for {
+      a <- stream
+      b <- squaredProperty(a)
+      if a == b
+    } yield (a, b)
+
+    assertContainsValues(combinedStream, List((1, 1)))
+  }
+
+  test("Filter and map over values of an Observable using collect") {
+    val stream = Bacon.fromArray[Int](js.Array(-5, 2, 17, -3))
+    val collectedStream = stream.collect {
+      case i if i > 0 => i * 2
+    }
+    assertContainsValues(collectedStream, List(4, 34))
   }
 }
